c4ebdc50b3e10beefb58b733e9af4127e4a8d809
Fernando Herrera <fherrera0206@gmail.com>
2022-02-22 11:41:25 -0500

    Create the component to add a book

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,17 @@
+const AddBook = () => (
+  <>
+    <h2>ADD NEW BOOK</h2>
+
+    <form action="">
+      <input type="text" />
+      <select name="" id="">
+        <option value="">Action</option>
+        <option value="">Terror</option>
+        <option value="">Comedy</option>
+      </select>
+      <button>Add Book</button>
+    </form>
+  </>
+);
+
+export default AddBook;
