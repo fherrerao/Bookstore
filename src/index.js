5ab26b7968e60b1a846af3a2abc4188695c76fa6
Fernando Herrera <fherrera0206@gmail.com>
2022-02-24 16:37:12 -0500

    dispatch getBook when the page load

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import { getBook } from './redux/books/books';
+
+store.dispatch(getBook);
 
 ReactDOM.render(
   <Provider store={store}>
