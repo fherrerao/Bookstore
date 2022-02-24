6e052fcdb3122d436a3b18fd2b052458363851eb
Fernando Herrera <fherrera0206@gmail.com>
2022-02-24 14:07:19 -0500

    Add fetch API to send the data to the server

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,9 @@
 const initialState = [];
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS_SUCCESS = 'bookStore/books/GET_BOOK';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ZF9EEG52YEvEwT4utW6R/books';
 
 // ACTIONS
 export const addBook = (payload) => ({
@@ -13,6 +16,32 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const sentBookAPI = (payload) => (
+  async (dispatch) => {
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: payload.id,
+        title: payload.title,
+        category: payload.category,
+      }),
+      headers: {
+        'Content-type': 'application/JSON',
+      },
+    });
+    dispatch(addBook(payload));
+  }
+);
+
+export const getBook = () => async (dispatch) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  dispatch({
+    type: GET_BOOKS_SUCCESS,
+    payload: data,
+  });
+};
+
 // REDUCERS
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -21,6 +50,9 @@ const booksReducer = (state = initialState, action) => {
 
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+
+    case GET_BOOKS_SUCCESS:
+      return [...state, action.payload];
 
     default:
       return state;
